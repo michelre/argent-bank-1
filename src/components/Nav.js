fa9f585c6b80c6705bbe6f620689b018817f9cc7
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-06 17:00:03 +0100

    Simple navigation functionalities

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,36 @@
-import React from 'react'
-import mainLogo from '../assets/argentBankLogo.png'
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import mainLogo from '../assets/argentBankLogo.png';
 
 const Nav = () => {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <Link to="/" className="main-nav-logo" >
         <img
           className="main-nav-logo-image"
           src={mainLogo}
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </Link>
+      {/* TO DO */}
       <div>
-        <a className="main-nav-item" href="./sign-in.html">
+        <NavLink to="./login" className="main-nav-item" >
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </NavLink>
       </div>
+      {/* TO DO */}
+      {/* <div>
+        <a className="main-nav-item" href="./user.html">
+          <i className="fa fa-user-circle"></i>
+          Tony
+        </a>
+        <a className="main-nav-item" href="./index.html">
+          <i className="fa fa-sign-out"></i>
+          Sign Out
+        </a>
+      </div> */}
     </nav>
   )
 }
