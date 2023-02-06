a4f8db89042bddbd3f861cce5e43658ce6ad773a
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2023-02-06 16:30:34 +0100

    Routing and static pages implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 function App() {
   return (
     <BrowserRouter >
-    <Nav />
-      <h1>ARGENT BANK</h1>
+      <Nav />
+      <Routes>
+        <Route index element= {<Home />} />
+        <Route path="login" element={<Login />}/>
+        <Route path="profile" element={<Profile />}/>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
